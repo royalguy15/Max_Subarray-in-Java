c04ebcb7fd847ba6eb1b40939ba71eb428f428d8
Rakesh Kumar Nayak <128237968+royalguy15@users.noreply.github.com>
2024-01-30 23:13:00 +0530

    Add files via upload

---
+++ max_subarray.java
@@ -0,0 +1,28 @@
+public class max_subarray {
+    public static void maxSubarraysSum(int numbers[]){
+        int currsum=0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for(int i=0;i<numbers.length;i++){
+            int start = i;
+            for(int j=i;j<numbers.length;j++){
+                int end = j;
+                currsum = 0;
+                for(int k=start;k<=end;k++){   //print
+                    currsum += numbers[k];
+                }
+                System.out.println(currsum);
+                if(maxsum<currsum){
+                    maxsum  = currsum;
+                }   
+            }
+        }
+        System.out.println("max sum = " + maxsum);
+    }
+
+    public static void main(String args[]){
+        int numbers[] = {2,4,6,8,10};
+        maxSubarraysSum(numbers);
+    }
+    
+}
